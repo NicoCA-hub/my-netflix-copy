ba7893a6fd0f883e9e04bbaf66efd78e2de4879d
NicoCA-hub <nicolascamargoamortegui@gmail.com>
2024-08-16 23:10:32 -0500

    V2

--- main_netflix.js
+++ main_netflix.js
@@ -8,19 +8,28 @@ const tabPPcontent= document.querySelector('#tab3');
 
 slcTab1.addEventListener('click', (e) => {
     tabCAcontent.style.display = "block";
+    slcTab1.classList.add('border');
     tabWAcontent.style.display = "none";
+    slcTab2.classList.remove('border');
     tabPPcontent.style.display = "none";
+    slcTab3.classList.remove('border');
 });
 
 slcTab2.addEventListener('click', (e) => {
     tabCAcontent.style.display = "none";
+    slcTab1.classList.remove('border');
     tabWAcontent.style.display = "block";
+    slcTab2.classList.add('border');
     tabPPcontent.style.display = "none";
+    slcTab3.classList.remove('border');
 })
 
 slcTab3.addEventListener('click', (e) => {
     tabCAcontent.style.display = "none";
+    slcTab1.classList.remove('border');
     tabWAcontent.style.display = "none";
+    slcTab2.classList.remove('border');
     tabPPcontent.style.display = "block";
+    slcTab3.classList.add('border');
 })
 
